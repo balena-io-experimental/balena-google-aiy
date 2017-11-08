506ed5e807b4a3365f0849db58cf764908376db5
curcuz <czkzcz.cmc@gmail.com>
2017-11-08 02:08:07 +0100

    init

---
+++ app/start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
+
+# Remove default audio
+rmmod snd_bcm2835  >/dev/null 2>&1 || true
+
+# Enable i2c and other interfaces
+modprobe i2c-dev || true
+
+# Configure assistant Client Credentials via ENV vars
+
+sed -i -e "s/CLIENT_ID/$CLIENT_ID/g" assistant.json
+sed -i -e "s/CLIENT_SECRET/$CLIENT_SECRET/g" assistant.json
+sed -i -e "s/PROJECT_ID/$PROJECT_ID/g" assistant.json
+
+# Start systemd services now that we have configured assistant Client Credentials
+systemctl enable alsa-init.service
+systemctl enable ntpdate.service
+systemctl enable voice-recognizer.service
